0f180bfdac2958178c301b1fa56c520ed01d4092
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-01 00:18:36 +0200

    [UPD] Added content to welcome.blade

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container-fluid">
+<div id="jumbotron">
+    <div class="row">
+        <div class="col-12">
+            <h2>Jumbotron</h2>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            
+        </div>    
+    </div>    
+</div>  
+    
+@endsection
