83563d2560f9a39892f36bdf4ca7628411d16341
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-01 00:56:21 +0200

    [FIX] FIxed table

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -14,6 +14,20 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config('comics');
+        foreach($comics as $comic){
+            $new_comic = new Comic();
+            $new_comic->title = $comic['title'];
+            $new_comic->description = $comic['description'];
+            $new_comic->thumb = $comic['thumb'];
+            $new_comic->price = $comic['price'];
+            $new_comic->sale_date = $comic['sale_date'];
+            $new_comic->type = $comic['type'];
+            $artists_string = implode(',', $comic['artists']);
+            $writers_string = implode(',', $comic['writers']);
+            $new_comic->save();
+
+        }
+            
     }
 }
