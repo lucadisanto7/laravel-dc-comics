3ff70b5b8dcde19ebbc7c7e0df0c035b581d43ee
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-01 01:13:41 +0200

    [UPD] Added Route for controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComicController as ComicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('homepage');
+
+Route::resource('/comics', ComicController::class);
