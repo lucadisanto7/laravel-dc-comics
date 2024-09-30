abbd35ecfaee1210414b8f6d2466d2a8d343f99f
lucadisanto7 <lucadisanto47@gmail.com>
2024-09-30 22:16:18 +0200

    [UPD] Added header img

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="logo">
+                        <img src=" {{ vite::asset('resources/img/dc-logo.png') }}" alt="comics">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
