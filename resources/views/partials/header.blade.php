3cebe9ba0ee2dab461acb0b436b036329862a8a2
lucadisanto7 <lucadisanto47@gmail.com>
2024-09-30 22:21:06 +0200

    [UPD] Added list

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,6 +6,20 @@
                     <div class="logo">
                         <img src=" {{ vite::asset('resources/img/dc-logo.png') }}" alt="comics">
                     </div>
+                    <div class="menu">
+                        <ul class="list-unstyled d-flex m-0">
+                            <li class="nav-item px-3 py-5"><a class="nav-link" href="">Characters</a></li>
+                            <li class="nav-item px-3 py-5"><a class="nav-link" href="">Comics</a></li>
+                            <li class="nav-item px-3 py-5"><a class="nav-link" href="">Movies</a></li>
+                            <li class="nav-item px-3 py-5"><a class="nav-link" href="">TV</a></li>
+                            <li class="nav-item px-3 py-5"><a class="nav-link" href="">Games</a></li>
+                            <li class="nav-item px-3 py-5"><a class="nav-link" href="">Collectibles</a></li>
+                            <li class="nav-item px-3 py-5"><a class="nav-link" href="">Videos</a></li>
+                            <li class="nav-item px-3 py-5"><a class="nav-link" href="">Fans</a></li>
+                            <li class="nav-item px-3 py-5"><a class="nav-link" href="">News</a></li>
+                            <li class="nav-item px-3 py-5"><a class="nav-link" href="">Shop</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
