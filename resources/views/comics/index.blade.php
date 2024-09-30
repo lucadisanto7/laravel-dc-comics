0c29318a9779d85e87e0c5468ced46cebb7fb6bd
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-01 01:25:07 +0200

    [UPD] Added comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -10,9 +10,14 @@
 </div>
 <div class="container">
     <div class="row">
+        @foreach ($comics as $comic)
         <div class="col-12">
-            @foreach ($comics as $comic)
-                
+            <div class="comic">
+                <div class="thumb">
+                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                </div>
+                <h4 class="text-uppercase">{{ $comic['title'] }}</h4>
+            </div>
             @endforeach
         </div>    
     </div>    
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,7 +9,7 @@
                     <div class="menu">
                         <ul class="list-unstyled d-flex m-0">
                             <li class="nav-item px-3 py-5"><a class="nav-link" href="">Characters</a></li>
-                            <li class="nav-item px-3 py-5 {{Route::currentRouteName() === 'comics' ? 'active' : ''}}"><a class="nav-link" href="">Comics</a></li>
+                            <li class="nav-item px-3 py-5 {{Route::currentRouteName() === 'comics' ? 'active' : ''}}"><a class="nav-link" href="{{ route ('comics.index')}}">Comics</a></li>
                             <li class="nav-item px-3 py-5"><a class="nav-link" href="">Movies</a></li>
                             <li class="nav-item px-3 py-5"><a class="nav-link" href="">TV</a></li>
                             <li class="nav-item px-3 py-5"><a class="nav-link" href="">Games</a></li>
