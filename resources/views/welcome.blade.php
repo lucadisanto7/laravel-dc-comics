d9de680106822da73113eb6b12d2af742e590bfb
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-01 00:29:10 +0200

    [UPD] Added style to jumbotron

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,13 @@
 <div id="jumbotron">
     <div class="row">
         <div class="col-12">
-            <h2>Jumbotron</h2>
         </div>
     </div>
 </div>
 <div class="container">
     <div class="row">
         <div class="col-12">
-            
+
         </div>    
     </div>    
 </div>  
