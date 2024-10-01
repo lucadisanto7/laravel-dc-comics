85073e89b2bb235751df88a037eb26c4103e62c3
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-01 09:56:33 +0200

    [UPD] Added comics.show to index blade

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -13,12 +13,14 @@
         <div class="row mt-100">
             @foreach ($comics as $comic)
             <div class="col-12 col-md-6 col-lg-2">
+                <a href="{{ route('comics.show', ['comic' => $comic->id])}}">
                 <div class="comic">
                     <div class="thumb">
                         <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
                     </div>
                     <h4 class="text-uppercase">{{ $comic['title'] }}</h4>
                 </div>
+                </a>    
             </div>    
             @endforeach
         </div>    
